9b0e0eb4a84903c4c223404c6565184e8ca6a60f
Thiago Frinhani <thiago.frinhani@sindifisconacional.org.br>
2022-01-03 20:03:00 -0300

    salvando log arquivo txt

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 setInterval( () => {
 
@@ -18,6 +19,8 @@ setInterval( () => {
     console.clear()
     console.log("=== PC STATS ===")
     console.table(stats)
+
+    log(`${JSON.stringify(stats)}\n`)
 
 },1000)
 
